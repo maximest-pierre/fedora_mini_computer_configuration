991f2e0f43368176088a8617842d64889b355cf2
mst-pierre <mst-pierre@localhost.localdomain>
2016-04-01 04:02:27 -0400

    Update script to install vim config

--- config.bash
+++ config.bash
@@ -9,6 +9,23 @@ dnf install i3 i3status i3lock -y
 dnf install -y xorg-x11-xinit xorg-x11-server-Xorg xorg-x11-drv-evdev xorg-x11-utils xorg-x11-font-utils xorg-x11-fonts-misc xorg-x11-drivers 
 dnf groupinstall Fonts -y
 
+cd ~
+
+# Install pip3
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo python2 get-pip.py
+
+# Dependencies for vim config
+sudo pip install dbgp vim-debug pep8 flake8 pyflakes isort
+sudo pip2 install dbgp vim-debug pep8 flake8 pyflakes isort
+
+# Download .vimrc 
+wget https://raw.githubusercontent.com/maximest-pierre/vim/master/vimrc -O $HOME/.vimrc
+
+# Install vim config
+vim -E -u $HOME/.vimrc +qall
+
 echo exec /usr/bin/i3 > .xinitrc
 
 startx

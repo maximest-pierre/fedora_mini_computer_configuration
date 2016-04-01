31435abdc6dfa8cabe05b946fbfa6e17f3d68c55
mst-pierre <mst-pierre@localhost.localdomain>
2016-04-01 03:43:01 -0400

    update config.bash because of typo

--- config.bash
+++ config.bash
@@ -5,7 +5,7 @@ sudo dnf install  http://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-23.noarch.rpm
 
 # Install i3, xorg-server and fonts
-nf install i3 i3status i3lock -y
+dnf install i3 i3status i3lock -y
 dnf install -y xorg-x11-xinit xorg-x11-server-Xorg xorg-x11-drv-evdev xorg-x11-utils xorg-x11-font-utils xorg-x11-fonts-misc xorg-x11-drivers 
 dnf groupinstall Fonts -y
 
